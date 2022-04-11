7d1786f7b7d65c5522852e6ec55e6b98a7ae4c6e
Bruno Rossetto <bruno.rossetto97@gmail.com>
2022-04-10 22:14:52 -0300

    Add log SchedulingService

--- src/main/java/com/bruno/telegram_bot/currencybot/configuration/CurrencyApiConfig.java
+++ src/main/java/com/bruno/telegram_bot/currencybot/configuration/CurrencyApiConfig.java
@@ -1,6 +1,5 @@
 package com.bruno.telegram_bot.currencybot.configuration;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/com/bruno/telegram_bot/currencybot/service/SchedulingService.java
+++ src/main/java/com/bruno/telegram_bot/currencybot/service/SchedulingService.java
@@ -1,8 +1,10 @@
 package com.bruno.telegram_bot.currencybot.service;
 
 import com.bruno.telegram_bot.currencybot.bot.CurrencyBot;
-import com.bruno.telegram_bot.currencybot.configuration.TelegramConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -13,25 +15,27 @@ import java.util.concurrent.ExecutionException;
 public class SchedulingService {
 
     private final CurrencyBot currencyBot;
-    private final TelegramConfig telegramConfig;
+    private final String chatId;
+    private final Logger logger = LoggerFactory.getLogger(SchedulingService.class);
 
     @Autowired
-    public SchedulingService(CurrencyBot currencyBot, TelegramConfig telegramConfig) {
+    public SchedulingService(CurrencyBot currencyBot, @Qualifier("getChatId") String chatId) {
         this.currencyBot = currencyBot;
-        this.telegramConfig = telegramConfig;
+        this.chatId = chatId;
     }
 
     // 9:00 every day
     @Scheduled(cron = "0 0 9 * * *", zone = "America/Sao_Paulo")
     private void notifyWithCurrenciesMorning() throws IOException, ExecutionException, InterruptedException {
-        currencyBot.sendCurrenciesTo(telegramConfig.getBotChatId());
+        logger.info("Sending morning message.");
+        currencyBot.sendCurrenciesTo(chatId);
     }
 
     // 21:00 every day
-    @Scheduled(cron = "0 10 21 * * *", zone = "America/Sao_Paulo")
+    @Scheduled(cron = "0 16 22 * * *", zone = "America/Sao_Paulo")
     private void notifyWithCurrenciesEvening() throws IOException, ExecutionException, InterruptedException {
-        currencyBot.sendCurrenciesTo(telegramConfig.getBotChatId());
+        logger.info("Sending night message.");
+        currencyBot.sendCurrenciesTo(chatId);
     }
-
 
 }
